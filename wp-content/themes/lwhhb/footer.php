43cc73201c3ea2f112cc4231da8b9fed44d1faf4
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-06-18 23:42:55 +0600

    7.4 - Separate header and footer files
    7.4 - Separate header and footer files

---
+++ wp-content/themes/lwhhb/footer.php
@@ -0,0 +1,58 @@
+    <!--footer start-->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-12">
+                    <div class="footer-info">
+                        <div class="f-logo">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" srcset="<?php echo get_template_directory_uri();?>/assets/img/logo@2x.png 2x" alt="">
+                        </div>
+                        <p>High school classmates of the gunman who killed nine people</p>
+                    </div>
+                    <div class="social-links">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-youtube"></i></a>
+                        <a href="#"><img src="<?php echo get_template_directory_uri();?>/assets/img/play-store.jpg" srcset="<?php echo get_template_directory_uri();?>/assets/img/play-store@2x.jpg 2x" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-md-2 col-6">
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms & Condition</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2 col-6">
+                    <h3>Features</h3>
+                    <ul>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Business</a></li>
+                        <li><a href="#">Sports</a></li>
+                        <li><a href="#">Travel</a></li>
+                        <li><a href="#">Sports</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2 col-6">
+                    <h3>More</h3>
+                    <ul>
+                        <li><a href="#">Photos</a></li>
+                        <li><a href="#">Video Gallery</a></li>
+                        <li><a href="#">Most Viewed</a></li>
+                        <li><a href="#">Most Popular</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--footer end-->
+
+<?php
+wp_footer();
+?>
+
+</body>
+</html>
